529b48fddc777283a70abc178194479fabd60730
Diogo Bombonatti <126741260+diogobombonatti@users.noreply.github.com>
2023-08-31 00:07:29 -0300

    teste

--- src/main/java/com/example/App_4.java
+++ src/main/java/com/example/App_4.java
@@ -7,7 +7,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
-import javafx.stage.Stage;
+
 
 public class App_4 extends Application {
 
